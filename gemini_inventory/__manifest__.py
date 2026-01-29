66c757763125c93611a6c88b2ef41e39c4eaed62
Raul Cid <tu_correo@ejemplo.com>
2026-01-29 20:43:39 +0000

    feat: Agregar configuración de API Key de Gemini en Settings

--- gemini_inventory/__manifest__.py
+++ gemini_inventory/__manifest__.py
@@ -3,12 +3,12 @@
     'name': 'Gemini Agente de inventario',
     'version': '19.0.1.0.0',
     'category': 'Productivity',
-
     'author': 'Raul Cid',
     'depends': ['base', 'mail', 'stock', 'product'],
     'data': [
         'data/discuss_channel_data.xml',
-          ],
+        'views/res_config_settings_views.xml',
+    ],
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
--- gemini_inventory/models/__init__.py
+++ gemini_inventory/models/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import discuss_channel
+from . import res_config_settings
